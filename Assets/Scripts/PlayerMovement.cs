d40461e7fa23661536508ec8fdcc8a2ec364e3f5
1nsp3c <82948413+1nsp3c@users.noreply.github.com>
2022-11-15 22:12:33 +0800

    Mobile Implementation
    Player movement on mobile with looking.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,9 +7,12 @@ public class PlayerMovement : MonoBehaviour
     [HideInInspector]
     public CharacterController mCharacterController;
     public Animator mAnimator;
+    public FixedJoystick mJoystick;
+    public bool mFollowCameraForward = false;
 
     public float mWalkSpeed = 1.0f;
     public float mRotationSpeed = 50.0f;
+    public float mTurnRate = 200.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +23,10 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float hInput = Input.GetAxis("Horizontal");
-        float vInput = Input.GetAxis("Vertical");
+        //float hInput = Input.GetAxis("Horizontal");
+        //float vInput = Input.GetAxis("Vertical");
+        float hInput = 2.0f * mJoystick.Horizontal;
+        float vInput = 2.0f * mJoystick.Vertical;
 
 
         float speed = mWalkSpeed;
@@ -33,7 +38,26 @@ public class PlayerMovement : MonoBehaviour
 
         if (mAnimator == null) return;
 
-        transform.Rotate(0.0f, hInput * mRotationSpeed * Time.deltaTime, 0.0f);
+        if (mFollowCameraForward)
+        {
+            if (Mathf.Abs(hInput) > 0.1f)
+            {
+                // rotate Player towards the camera forward.
+                Vector3 eu = Camera.main.transform.rotation.eulerAngles;
+                transform.rotation = Quaternion.RotateTowards(
+                    transform.rotation,
+                    Quaternion.Euler(0.0f, eu.y, 0.0f),
+                    mTurnRate * Time.deltaTime);
+            }
+        }
+        else
+        {
+            if (Mathf.Abs(hInput) > 0.1f)
+            {
+                transform.Rotate(0.0f, hInput * mRotationSpeed * Time.deltaTime, 0.0f);
+            }
+        }
+
 
         Vector3 forward = transform.forward;
         forward.y = 0.0f;
--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class ThirdPersonCamera : MonoBehaviour
 {
     public Transform mPlayer;
+    public CameraType mCameraType = CameraType.Follow_Track_Pos;
+    Dictionary<CameraType, TPCBase> mThirdPersonCameraDict = new Dictionary<CameraType, TPCBase>();
+    public FixedTouchField mTouchField;
+
 
     TPCBase mThirdPersonCamera;
     public Vector3 mPositionOffset = new Vector3(0.0f, 2.0f, -2.5f);
     public Vector3 mAngleOffset = new Vector3(0.0f, 0.0f, 0.0f);
     public float mDamping = 1.0f;
+    public float mMinPitch = -30.0f;
+    public float mMaxPitch = 30.0f;
+    public float mRotationSpeed = 50.0f;
+
 
     void Start()
     {
@@ -17,13 +25,41 @@ public class ThirdPersonCamera : MonoBehaviour
         // Set the game constant parameters to the GameConstants class.
         GameConstants.Damping = mDamping;
         GameConstants.CameraAngleOffset = mAngleOffset;
+        GameConstants.MinPitch = mMinPitch;
+        GameConstants.MaxPitch = mMaxPitch;
+        GameConstants.RotationSpeed = mRotationSpeed;
         //mThirdPersonCamera = new TPCTrack(transform, mPlayer);
         //mThirdPersonCamera = new TPCTrack(transform, mPlayer); 
-        //mThirdPersonCamera = new TPCFollowTrackPositionAndRotation(transform, mPlayer);
-        mThirdPersonCamera = new TPCTopDown(transform, mPlayer);
+        mThirdPersonCamera = new TPCFollowTrackPositionAndRotation(transform, mPlayer);
+        //mThirdPersonCamera = new TPCTopDown(transform, mPlayer);
+        mThirdPersonCameraDict.Add(CameraType.Track, new TPCTrack(transform, mPlayer));
+        mThirdPersonCameraDict.Add(CameraType.Follow_Track_Pos, new TPCFollowTrackPosition(transform, mPlayer));
+        mThirdPersonCameraDict.Add(CameraType.Follow_Track_Pos_Rot, new TPCFollowTrackPositionAndRotation(transform, mPlayer));
+        mThirdPersonCameraDict.Add(CameraType.Topdown, new TPCTopDown(transform, mPlayer));
+        mThirdPersonCameraDict.Add(CameraType.Follow_Independent, new TPCFollowIndependentRotation(transform, mPlayer, mTouchField));
+        mThirdPersonCamera = mThirdPersonCameraDict[mCameraType];
 
     }
+    private void Update()
+    {
+        GameConstants.CameraPositionOffset = mPositionOffset;
+        // Set the game constant parameters to the GameConstants class.
+        GameConstants.Damping = mDamping;
+        GameConstants.CameraAngleOffset = mAngleOffset;
+        mThirdPersonCamera = mThirdPersonCameraDict[mCameraType];
+        GameConstants.MinPitch = mMinPitch;
+        GameConstants.MaxPitch = mMaxPitch;
+        GameConstants.RotationSpeed = mRotationSpeed;
 
+    }
+    public enum CameraType
+    {
+        Track,
+        Follow_Track_Pos,
+        Follow_Track_Pos_Rot,
+        Topdown,
+        Follow_Independent, //we have not implemented this mode yet
+    }
 
     void LateUpdate()
     {
@@ -95,6 +131,9 @@ public class ThirdPersonCamera : MonoBehaviour
         public static Vector3 CameraAngleOffset { get; set; }
         public static Vector3 CameraPositionOffset { get; set; }
         public static float Damping { get; set; }
+        public static float RotationSpeed { get; set; }
+        public static float MinPitch { get; set; }
+        public static float MaxPitch { get; set; }
     }
     public abstract class TPCFollow : TPCBase
     {
@@ -185,6 +224,62 @@ public class ThirdPersonCamera : MonoBehaviour
             base.Update();
         }
     }
+    public class TPCFollowIndependentRotation : TPCBase
+    {
+        FixedTouchField mTouchField;
+        private float angleX = 0.0f;
 
+        public TPCFollowIndependentRotation(Transform cameraTransform, Transform playerTransform)
+            : base(cameraTransform, playerTransform)
+        {
+        }
+
+#if UNITY_ANDROID
+        public TPCFollowIndependentRotation(Transform cameraTransform, Transform playerTransform, FixedTouchField fixedTouch)
+            : base(cameraTransform, playerTransform)
+        {
+            mTouchField = fixedTouch;
+        }
+#endif
+
+        public override void Update()
+        {
+            float mx, my;
+            mx = mTouchField.TouchDist.x * Time.deltaTime;
+            my = mTouchField.TouchDist.y * Time.deltaTime;
+
+            // We apply the initial rotation to the camera.
+            Quaternion initialRotation = Quaternion.Euler(GameConstants.CameraAngleOffset);
+
+            Vector3 eu = mCameraTransform.rotation.eulerAngles;
+
+            angleX -= my * GameConstants.RotationSpeed;
+
+            // We clamp the angle along the Xaxis to be between the min and max pitch.
+            angleX = Mathf.Clamp(angleX, GameConstants.MinPitch, GameConstants.MaxPitch);
+
+            eu.y += mx * GameConstants.RotationSpeed;
+            Quaternion newRot = Quaternion.Euler(angleX, eu.y, 0.0f) * initialRotation;
+
+            mCameraTransform.rotation = newRot;
+
+            Vector3 forward = mCameraTransform.rotation * Vector3.forward;
+            Vector3 right = mCameraTransform.rotation * Vector3.right;
+            Vector3 up = mCameraTransform.rotation * Vector3.up;
+
+            Vector3 targetPos = mPlayerTransform.position;
+            Vector3 desiredPosition = targetPos
+                + forward * GameConstants.CameraPositionOffset.z
+                + right * GameConstants.CameraPositionOffset.x
+                + up * GameConstants.CameraPositionOffset.y;
+
+            Vector3 position = Vector3.Lerp(mCameraTransform.position,
+                desiredPosition,
+                Time.deltaTime * GameConstants.Damping);
+
+            mCameraTransform.position = position;
+
+        }
+    }
 
 }
